753b8b2b507b467e338035a7e0ae81c501aa7877
Krista Williams <rystakei@gmail.com>
2013-07-26 17:16:22 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lingodex::Application.config.secret_token = '591a7bb97a84dff4e5b27ccd195ce798fc5a1cc3955db118f4aa787e9ae21615ef90326154f7ee891825f299781c38664e891aa95a7785b28bf38e05c986f237'
