3e8093fda69b1d4456e50621002837714147e210
MakerSquare Student <students@themakersquare.com>
2013-08-03 10:02:42 -0500

    Fixed routing error - Now the user is able to sign out

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,23 @@ u = User.new
 d1 = Deck.new
 d1.subject = "French"
 d1.user_id = 1
+u.save
+d1.save
 
 c1 = Card.new
 c1.term = "boire"
 c1.definition = "to drink"
 c1.deck_id = 1
+c1.save
 
 c2 = Card.new
 c2.term = "croire"
 c2.definition = "to believe"
 c2.deck_id = 1
+c2.save
 
 c3 = Card.new
 c3.term = "avoir"
 c3.definition = "to have"
 c3.deck_id = 1
+c3.save
